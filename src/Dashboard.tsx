803f0dadba7fb0483d86ceb9ac1254b2454face0
PraneethGunas <praneethgunas404@gmail.com>
2024-10-13 18:19:26 -0400

    Integration: Added entity controls custom to local ha

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,14 +1,24 @@
-import { Column, TimeCard } from '@hakit/components';
-import { useHass } from "@hakit/core";
+import { Column, MediaPlayerCard, TimeCard, WeatherCard } from '@hakit/components';
+import { useHass } from '@hakit/core';
 
 function Dashboard() {
   const { getAllEntities } = useHass();
-  return <Column fullWidth fullHeight>
-    <h2>Succesfully Authenticated!</h2>
-    <p>The time below should be updating from home asisstant every minute</p>
-    <TimeCard />
-    <p>You have <b>{Object.keys(getAllEntities()).length}</b> entities to start automating with! Have fun!</p>
-  </Column>
+  const entities = getAllEntities();
+  console.log(entities);
+  return (
+    <Column fullWidth>
+      <h2>Succesfully Authenticated!</h2>
+      <p>The time below should be updating from home asisstant every minute</p>
+      <TimeCard />
+      <br />
+      <WeatherCard entity='weather.forecast_home' />
+      <br />
+      <MediaPlayerCard entity='media_player.work_station' layout='slim' />
+      <p>
+        You have <b>{Object.keys(entities).length}</b> entities to start automating with! Have fun!
+      </p>
+    </Column>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
