37db9ec8a4a6f2453e181b6c75ddf9a6bea0ed9c
PraneethGunas <praneethgunas404@gmail.com>
2024-11-04 16:54:21 -0500

    feat: add VirtualSensors component to manage and display virtual sensor controls

---
+++ src/components/VirtualSensors.tsx
@@ -0,0 +1,42 @@
+import { Column, Group, RangeSlider, SensorCard } from '@hakit/components';
+import { useVirtual } from '../Hooks/useVirtual';
+import { useCallback } from 'react';
+import { useHass } from '@hakit/core';
+
+export const VirtualSensors = () => {
+  const { callService } = useHass();
+  const { virtualObjects } = useVirtual();
+  const sensors = virtualObjects.filter(obj => obj.entity_id.includes('sensor.'));
+
+  const updateTemperature = useCallback((entity_id: string, value: number) => {
+    callService({
+      domain: 'virtual',
+      service: 'set',
+      serviceData: {
+        value,
+      },
+      target: {
+        entity_id,
+      },
+    });
+  }, []);
+
+  return (
+    <Group title='Virtual Sensors'>
+      {sensors.map(sensor => (
+        <Column fullWidth>
+          <SensorCard entity={sensor.entity_id} />
+          <RangeSlider
+            min={-20}
+            max={100}
+            step={1}
+            value={Number(sensor.state)}
+            onChange={value => {
+              updateTemperature(sensor.entity_id, value);
+            }}
+          />
+        </Column>
+      ))}
+    </Group>
+  );
+};
