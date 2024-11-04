598cd835293968a63a3ab9bb622378c83490a515
PraneethGunas <praneethgunas404@gmail.com>
2024-11-04 16:54:09 -0500

    feat: update Dashboard component to include VirtualLights and VirtualSensors

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,23 +1,23 @@
-import { Column, MediaPlayerCard, TimeCard, WeatherCard } from '@hakit/components';
+import { TimeCard } from '@hakit/components';
 import { useHass } from '@hakit/core';
+import { VirtualLights } from './components/VirtualLights';
+import { VirtualSensors } from './components/VirtualSensors';
 
 function Dashboard() {
   const { getAllEntities } = useHass();
   const entities = getAllEntities();
-  console.log(entities);
+
   return (
-    <Column fullWidth>
-      <h2>Succesfully Authenticated!</h2>
-      <p>The time below should be updating from home asisstant every minute</p>
+    <>
       <TimeCard />
       <br />
-      <WeatherCard entity='weather.forecast_home' />
+      <VirtualLights />
       <br />
-      <MediaPlayerCard entity='media_player.work_station' layout='slim' />
+      <VirtualSensors />
       <p>
         You have <b>{Object.keys(entities).length}</b> entities to start automating with! Have fun!
       </p>
-    </Column>
+    </>
   );
 }
 
