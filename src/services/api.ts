28b6905f19f802e9a19960d1015b92cc5f8d04ef
PraneethGunas <praneethgunas404@gmail.com>
2024-11-16 15:54:52 -0500

    feat: update API service to include automation entry endpoint and refactor virtual device path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://hammock.local:8123';
-const VIRTUAL_SEVICE = '/api/virtual';
+const API = '/api';
+const VIRTUAL_SEVICE = '/virtual';
 
-const ADD_VIRTUAL_DEVICE = `${BASE_URL}${VIRTUAL_SEVICE}/add`;
+const ADD_VIRTUAL_DEVICE = `${BASE_URL}${API}${VIRTUAL_SEVICE}/add`;
+const AUTOMATION_ENTRY = `${BASE_URL}${API}/config/automation/config/entry`;
 export const addEntity = async (config: any) => {
   return await axios.post(ADD_VIRTUAL_DEVICE, config, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_HA_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const createAutomation = async (automationConfig: any) => {
+  return await axios.post(AUTOMATION_ENTRY, automationConfig, {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_HA_TOKEN}`,
       'Content-Type': 'application/json',
