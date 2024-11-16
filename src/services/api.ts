dc8712f32816a41da6fb55a1247243963cf0559a
PraneethGunas <praneethgunas404@gmail.com>
2024-11-16 15:59:44 -0500

    feat: add validateAutomation function to API service for configuration validation

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,8 @@ const VIRTUAL_SEVICE = '/virtual';
 
 const ADD_VIRTUAL_DEVICE = `${BASE_URL}${API}${VIRTUAL_SEVICE}/add`;
 const AUTOMATION_ENTRY = `${BASE_URL}${API}/config/automation/config/entry`;
+const VALIDATE_AUTOMATION = `${BASE_URL}${API}/config/core/check_config`;
+
 export const addEntity = async (config: any) => {
   return await axios.post(ADD_VIRTUAL_DEVICE, config, {
     headers: {
@@ -17,6 +19,15 @@ export const addEntity = async (config: any) => {
 
 export const createAutomation = async (automationConfig: any) => {
   return await axios.post(AUTOMATION_ENTRY, automationConfig, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_HA_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const validateAutomation = async () => {
+  return await axios.post(VALIDATE_AUTOMATION, {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_HA_TOKEN}`,
       'Content-Type': 'application/json',
