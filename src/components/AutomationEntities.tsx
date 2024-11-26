522ab9ef628932b4f2168675b079104293ad48dc
PraneethGunas <praneethgunas404@gmail.com>
2024-11-26 11:43:21 -0500

    feat: add overlay for displaying automation script with CodeBlock component; enhance AutomationEntities layout

--- src/components/AutomationEntities.tsx
+++ src/components/AutomationEntities.tsx
@@ -3,6 +3,7 @@ import { type EntityName } from '@hakit/core';
 import { Column, Row, EntitiesCardRow, ButtonCard } from '@hakit/components';
 import { useEntitiesByDomain } from '../Hooks/useEntitiesByDomain';
 import { useState } from 'react';
+import CodeBlock from './CodeBlock';
 
 const EntityMap = (entity_id: EntityName) => {
   const [active, setActive] = useState(entity_id);
@@ -37,41 +38,62 @@ const EntityMap = (entity_id: EntityName) => {
 export const AutomationEntities = props => {
   const { automation, triggeredAutomation, entity } = props;
   const automationEntities = extractCategorizedEntityIds(automation);
+  const [showScript, setShowScript] = useState(false);
+  const toggleScript = () => {
+    setShowScript(!showScript);
+  };
+
+  const onClose = () => {
+    setShowScript(false);
+  };
 
   return (
-    <ButtonCard entity={entity.entity_id} style={{ background: triggeredAutomation?.name == automation.alias ? 'green' : undefined }}>
-      <div>
-        {/* Trigger Entities */}
-        {automationEntities['trigger entities'].length > 0 ? (
-          <div>
-            <Row>
-              <h5>Entities at Trigger</h5>
-            </Row>
-            <Column>{automationEntities['trigger entities'].map(entity_id => EntityMap(entity_id))}</Column>
-          </div>
-        ) : null}
+    <>
+      <ButtonCard
+        entity={entity.entity_id}
+        style={{ background: triggeredAutomation?.name == automation.alias ? 'green' : undefined }}
+        onClick={toggleScript}
+      >
+        <div>
+          {/* Trigger Entities */}
+          {automationEntities['trigger entities'].length > 0 ? (
+            <div>
+              <Row>
+                <h5>Entities at Trigger</h5>
+              </Row>
+              <Column>{automationEntities['trigger entities'].map(entity_id => EntityMap(entity_id))}</Column>
+            </div>
+          ) : null}
 
-        {/* Condition Entities */}
-        {automationEntities['condition entities'].length > 0 ? (
-          <div>
-            <Row>
-              <h5>Entities at Condition</h5>
-            </Row>
-            <Column>{automationEntities['condition entities'].map(entity_id => EntityMap(entity_id))}</Column>
-          </div>
-        ) : null}
+          {/* Condition Entities */}
+          {automationEntities['condition entities'].length > 0 ? (
+            <div>
+              <Row>
+                <h5>Entities at Condition</h5>
+              </Row>
+              <Column>{automationEntities['condition entities'].map(entity_id => EntityMap(entity_id))}</Column>
+            </div>
+          ) : null}
 
-        {/* Action Entities */}
-        {automationEntities['action entities'].length > 0 ? (
-          <div>
-            <Row>
-              <h5>Entities at Action</h5>
-            </Row>
-            <Column>{automationEntities['action entities'].map(entity_id => EntityMap(entity_id))}</Column>
+          {/* Action Entities */}
+          {automationEntities['action entities'].length > 0 ? (
+            <div>
+              <Row>
+                <h5>Entities at Action</h5>
+              </Row>
+              <Column>{automationEntities['action entities'].map(entity_id => EntityMap(entity_id))}</Column>
+            </div>
+          ) : null}
+          <br />
+        </div>
+      </ButtonCard>
+      {showScript ? (
+        <div className='overlay' onClick={onClose}>
+          <div className='overlay-content'>
+            <CodeBlock code={automation} />
           </div>
-        ) : null}
-        <br />
-      </div>
-    </ButtonCard>
+        </div>
+      ) : null}
+    </>
   );
 };
